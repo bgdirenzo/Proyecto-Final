b9a9d2e3a679bc13d076de6bb6f054f50da269ac
bdirenzo <barbara.direnzo@gba.gob.ar>
2023-05-08 13:49:17 -0300

    envio de formulario

--- estudioEme/back/routes/api.js
+++ estudioEme/back/routes/api.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var clientesModel = require('../models/clientesModel');
+var nodemailer = require('nodemailer');
 
+/*GET Home Page*/
 router.get('/clientes', async function (req, res, next) {
     var clientes = await clientesModel.getClientes();
 
     res.json(clientes);
 });
+
+/*POST Home Page*/
+router.post('/contacto', async (req,res)=> {
+    const mail = {
+      to: 'barbara.direnzo@gba.gob.ar',
+      subject: 'Contacto a EME',
+      html: `${req.body.nombre} se contactó a traves de la web y quiere más información a este correo: ${req.body.email}
+            <br/> Además, deja el siguiente mensaje: ${req.body.mensaje}`
+    }
+  
+    const transport = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    });
+  
+    await transport.sendMail(mail)
+  
+    res.status(201).json({
+      error: false,
+      message: 'Mensaje enviado'
+    });
+  
+  });
 
 module.exports = router;
--- estudioEme/front/src/pages/ContactoPage.js
+++ estudioEme/front/src/pages/ContactoPage.js
@@ -1,29 +1,63 @@
 import React from "react";
 import '../styles/components/pages/ContactoPage.css';
-
+import { useState } from "react";
+import axios from 'axios';
 
 const ContactoPage = (props) => {
+
+    const initialFrom = {
+        nombre: '',
+        email: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialFrom);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true);
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialFrom)
+        }
+    }
+
     return (
-        <main class="holder contacto" style={{marginTop: 15 + 'px'}}>
-        <div>
-            <h2>Contactanos!</h2>
-            <form action="" method="" class="formulario">
-                <p>
-                    <label for="nombre">Nombre:</label>
-                    <input type="text" name="" />
-                </p>
-                <p>
-                    <label for="email">Email:</label>
-                    <input type="text" name="" />
-                </p>
-                <p>
-                    <label for="mensaje">Mensaje:</label>
-                    <textarea name=""></textarea>
-                </p>
-                <p class="acciones"><input type="submit" value="Enviar" /></p>
-            </form>
-        </div>
-    </main>
+        <main class="holder contacto" style={{ marginTop: 15 + 'px' }}>
+            <div>
+                <h2>Contactanos!</h2>
+                <form action="/contacto" method="post" onSubmit={handleSubmit} class="formulario">
+                    <p>
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} />
+                    </p>
+                    <p>
+                        <label for="email">Email:</label>
+                        <input type="text" name="email" value={formData.email} onChange={handleChange} />
+                    </p>
+                    <p>
+                        <label for="mensaje">Mensaje:</label>
+                        <textarea name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
+                    </p>
+                    <p class="acciones"><input type="submit" value="Enviar" /></p>
+                </form>
+                {sending ? <p>Enviando..</p> : null}
+                {msg ? <p>{msg}</p> : null}
+            </div>
+        </main>
     );
 }
 
